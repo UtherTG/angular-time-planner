c89dbb7d45d623a6ea80504992d7a851cd4fc9cd
UtherTG <uther.the.underdark@gmail.com>
2017-10-09 00:08:05 +0500

    chore: tests

---
+++ src/timePlannerContainer/tests/_data.mock.js
@@ -0,0 +1,15 @@
+export const mockData = {
+  rows: [],
+  options: {
+    type: 'histogram',
+    needCounter: true,
+    timeScope: 'day',
+    from: 1507230000000,
+    to: 1507316399999,
+    locale: {
+      labels: {
+        rowTitle: 'Users'
+      }
+    }
+  }
+};
